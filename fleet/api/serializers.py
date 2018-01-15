15470e5c80d1e3cd18f35f31d4f1954a58697fc6
uzayr <uzair_y3k@live.com>
2018-01-15 15:01:52 +0530

    Added Fleet api's crud

---
+++ fleet/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from fleet.models import (
+    FleetOwner, CommissionAgent, Operation, Feed, Vehicle
+)
+
+
+class FleetOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FleetOwner
+        fields = ("id", "name", "address", "email", "phone", "meta")
+
+
+class CommissionAgentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommissionAgent
+        fields = ("id", "name", "phone", "email", "hub", "meta")
+
+
+class OperationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Operation
+        fields = (
+            "id", "commission_agent", "fleet_owner", "source", "destination"
+        )
+
+
+class FeedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feed
+        fields = ("id", "vehicle", "city")
+
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = (
+            "id", "name", "brand", "model", "model_number", "registration_number"
+            "chasis_number", "engine_number", "body_type", "fuel_type", "color",
+            "meta", "fleet_owner",
+        )
