874e9db398dc0fc73bd0930d77be5ab763c30001
uzayr <uzair_y3k@live.com>
2018-01-15 15:00:03 +0530

    Transfer vehicle from common to fleet app

--- common/admin.py
+++ common/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import City, State, Country, Vehicle
+from .models import City, State, Country
 
 # Register your models here.
 admin.site.register(City)
 admin.site.register(State)
 admin.site.register(Country)
-admin.site.register(Vehicle)
