523e24e201b4aa9aee3de3df82ad87e0140f8104
uzayr <uzair_y3k@live.com>
2018-03-23 12:01:15 +0530

    Auth token with CA data

--- switchboard/views.py
+++ switchboard/views.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CAAuthTokenSerializer
+from agent.api.serializers import CommissionAgentSerializer
 
 class GetAuthToken(APIView):
     throttle_classes = ()
@@ -17,13 +18,15 @@ class GetAuthToken(APIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
+        ca = CommissionAgentSerializer(user.commissionagent)
         (user.__dict__).pop("_state")
         (user.__dict__).pop("backend")
         (user.__dict__).pop("password")
         return Response(
             {
                 "token": token.key,
-                "user": user.__dict__
+                "user": user.__dict__,
+                "ca": ca.data
             }
         )
 
