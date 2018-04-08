066193b2bfb388a37ebad1812edc8121642fa166
uzayr <uzair_y3k@live.com>
2018-04-08 20:01:34 +0530

    Added migrate command

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-python3 manage.py loaddata countries
-python3 manage.py loaddata states
-python3 manage.py loaddata materialtype
-python3 manage.py loaddata jobstatus
+docker-compose exec web python manage.py migrate
+docker-compose exec web python manage.py loaddata countries
+docker-compose exec web python manage.py loaddata states
+docker-compose exec web python manage.py loaddata materialtype
+docker-compose exec web python manage.py loaddata jobstatus
