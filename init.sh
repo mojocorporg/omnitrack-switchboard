3075b74d95d8e50b3ba7c22649a8b44d611dca24
uzayr <uzair_y3k@live.com>
2018-04-08 21:32:48 +0530

    Added fixture in init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ docker-compose exec web python manage.py loaddata countries
 docker-compose exec web python manage.py loaddata states
 docker-compose exec web python manage.py loaddata materialtype
 docker-compose exec web python manage.py loaddata jobstatus
+docker-compose exec web python manage.py loaddata vehicletype
