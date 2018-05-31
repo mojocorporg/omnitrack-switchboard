f15451cef7e94f664c9a3d21fba5a4346540023a
uzayr <uzair_y3k@live.com>
2018-05-31 09:27:04 +0530

    Added increased tonnage, users and leads

--- stats/api/viewsets.py
+++ stats/api/viewsets.py
@@ -4,6 +4,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from django.utils import timezone
 from datetime import timedelta
+from fleet.models import Lead, VehicleType
+from django.db.models import Sum
 
 
 class UserIncreasedViewSet(viewsets.ViewSet):
@@ -25,10 +27,16 @@ class TonnageIncreasedViewSet(viewsets.ViewSet):
     A simple ViewSet for listing or retrieving tonnage.
     """
     def retrieve(self, request):
+        total_tonnage = (
+            Lead.objects.all().aggregate(Sum("vehicle_type__weight"))
+        )["vehicle_type__weight__sum"]
+        tonnage_increased = (Lead.objects.all().filter(
+            created_at__gte=(timezone.now() - timedelta(days=7))
+        ).aggregate(Sum("vehicle_type__weight")))["vehicle_type__weight__sum"]
         return Response(
             {
-                "total": 0,
-                "last_week": 0
+                "total": total_tonnage if total_tonnage is not None else 0,
+                "last_week": tonnage_increased if tonnage_increased is not None else 0
             }
         )
 
@@ -37,10 +45,14 @@ class LeadIncreasedViewSet(viewsets.ViewSet):
     A simple ViewSet for listing or retrieving leads.
     """
     def retrieve(self, request):
+        total_lead = Lead.objects.all().count()
+        lead_increased = Lead.objects.all().filter(
+            created_at__gte=(timezone.now() - timedelta(days=7))
+        ).count()
         return Response(
             {
-                "total": 0,
-                "last_week": 0
+                "total": total_lead,
+                "last_week": lead_increased
             }
         )
 
